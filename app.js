3ec34f51ed2a5e5521df44392150b2f1f77aaab6
abhishek1886 <guptaabhishek1886@gmail.com>
2023-10-01 19:28:42 +0530

    assign8

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  //process.exit();
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    let message;
+    try {
+      message = fs.readFileSync("./message.txt", "utf-8");
+    } catch (err) {
+      console.error(err);
+      message = "Error reading the file.";
+    }
+
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(`<body><p>${message}</p><form action="/message" method="POST" ><input type="text" name="message" /><button type="submit">Send</button></form></body>`);
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFile('message.txt', message, (err) => {
+        res.writeHead(302, { Location: "/" });
+        return res.end();
+      });
+    })
+  }
+
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My App</title></head>");
-  res.write(
-    `<body><h1>${
-      req.url === "/home"
-        ? "Welcome Home!"
-        : req.url === "/about"
-        ? "Welcome to About Us Page!"
-        : req.url === "/node"
-        ? "Welcome to my Node Project!"
-        : ""
-    }</h1></body>`
-  );
+  res.write(`<body><h1>Hello</h1></body>`);
   res.write("</html>");
   res.end();
 });
