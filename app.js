2e1bcaf154b4a960a07902283163bf018e5ce67e
abhishek1886 <guptaabhishek1886@gmail.com>
2023-10-01 13:19:00 +0530

    Request/Response

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log('Abhishek');
+  console.log(req.url, req.method, req.headers);
+  //process.exit();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My App</title></head>");
+  res.write(
+    `<body><h1>${
+      req.url === "/home"
+        ? "Welcome Home!"
+        : req.url === "/about"
+        ? "Welcome to About Us Page!"
+        : req.url === "/node"
+        ? "Welcome to my Node Project!"
+        : ""
+    }</h1></body>`
+  );
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(4000);
