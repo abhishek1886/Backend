06f091e964bf824dac88eb6a45fb5cc133d797c3
abhishek1886 <guptaabhishek1886@gmail.com>
2023-10-07 15:38:03 +0530

    assign3

--- app.js
+++ app.js
@@ -3,19 +3,16 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use("/add-product", (req, res, next) => {
-  res.send("<form action='/product' method='POST'><input type='text' name='product' /><input type='number' name='quantity' /><button type='submit'>Add Product</button></form>");
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>404</h1><p>Page not found</p>");
 })
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Home route</h1>");
-});
 
 app.listen(5000);
